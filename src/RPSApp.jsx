2f8c33198e036a787a0fd6c3a5d3a68ddbb7f9df
Diego <daaron.macias@gmail.com>
2025-04-03 18:47:09 -0600

    Implementation of the useGame hook

--- src/RPSApp.jsx
+++ src/RPSApp.jsx
@@ -1,16 +1,25 @@
 import { Buttons } from "./components/Buttons";
 import { GameZone } from "./components/GameZone";
+import { useGame } from "./hooks/useGame";
 
 
 function RPSApp() {
+
+  const {
+    playerHand,
+    playerCounter,
+    computerHand,
+    computerCounter,
+    playGame
+  } = useGame();
 
   return (
     <>
       <h1 className="title">Rock, Paper, Scissors</h1>
       
-      <GameZone />
+      <GameZone playerCounter={playerCounter} playerHand={playerHand} computerHand={computerHand} computerCounter={computerCounter} />
       
-      <Buttons />
+      <Buttons game={playGame}/>
     </>
   )
 }
